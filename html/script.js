071981cb97545a84349d36f35f2ed2f9e274b56c
deivuks420xyz <83653223+deivuks420yxz@users.noreply.github.com>
2022-06-01 23:05:21 +0300

    Fixed bugs
    Fixed js bugs(not opening menu and other issues)

--- html/script.js
+++ html/script.js
@@ -1,11 +1,22 @@
-window.addEventListener("message", function(event) {
-    e = event.data
-    if(e.show == true) {
-        if (e.type == "updateData") {
+$(function() {
+    window.addEventListener("message", function(event) {
+        e = event.data
+        console.log(JSON.stringify(e))
+        if(e.show) {
             $(".balance-body-text2").html(e.balance)
-            $(".person-name").html(e.firstname + e.lastname)
+            $(".person-name").html(e.firstname + ' ' + e.lastname)
+            console.log(e.transactions.length)
+            if (e.transactions) {
+                $('.transactions-body-outlines').html(``)
+                for (let i = 0; i < e.transactions.length; i++) {
+                    $('.transactions-body-outlines').append(`<span>${e.transactions[i].message}</span>`)
+                }
+            }
+            $(".container").fadeIn(500)
+        } else {
+            $(".container").fadeOut(500)
         }
-    }
+    });
 });
 
 $(".withdraw-button1").click(function() {
@@ -15,13 +26,14 @@ $(".withdraw-button1").click(function() {
 });
 
 $(".deposit-button1").click(function() {
+    console.log($("#deposit-amount").val())
     $.post(`https://${GetParentResourceName()}/deposit`, JSON.stringify({
         amount: $("#deposit-amount").val()
     }));
 });
 
 $(".transfer-button1").click(function() {
-    $.post(`https://${GetParentResourceName()}/deposit`, JSON.stringify({
+    $.post(`https://${GetParentResourceName()}/transfer`, JSON.stringify({
         target: $("#transfer-player-id").val(),
         amount: $("#transfer-amount").val()
     }));
@@ -29,10 +41,7 @@ $(".transfer-button1").click(function() {
 
 $(document).keyup(function(e) {
     if (e.keyCode === 27) {
-
         $.post(`https://${GetParentResourceName()}/close`, JSON.stringify({}));
-        $(".container").fadeOut()
     }
-
-
 });
+
