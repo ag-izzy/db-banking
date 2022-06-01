2b7e4389bd20f1a5b069c9863216a748728ca6c9
deivuks420xyz <83653223+deivuks420yxz@users.noreply.github.com>
2022-06-01 23:02:53 +0300

    Added qbus core integration and fixed bugs
    Most off server side code was rewritten and fixed.

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,116 +1,104 @@
-if Config["Framework"] == "esx" then
+if Config.Framework == 'esx' then
+    local ESX = exports['es_extended']:getSharedObject()
 
-ESX = exports["es_extended"]:getSharedObject()
-local transactions
-
-RegisterServerEvent("db-banking:openBank", function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    exports.oxmysql:query("SELECT * FROM _bankTransactions WHERE identifier = @identifier", {
-        ['@identifier'] = xPlayer.getIdentifier()
-    }, function(r)
-        if r then
-            for _,v in pairs(r) do
-                transactions = v.msg
+    ESX.RegisterServerCallback('db-bank:GetData', function(source, cb)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local transactions = {}
+        exports.oxmysql:query('SELECT * FROM db_transactions WHERE identifier = @identifier', {
+            ['@identifier'] = xPlayer.getIdentifier()
+        }, function(r)
+            if r then
+                table.insert(transactions, result[i])
             end
+        end)
+        cb({firstname = xPlayer.firstName, lastname = xPlayer.lastName, balance = xPlayer.getAccount('bank').money, transactions = transactions})
+    end)
+
+    RegisterNetEvent('db-banking:withdraw', function(amount)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer.getAccount('bank').money >= amount then
+            xPlayer.removeAccountMoney('bank', amount)
+            xPlayer.addAccountMoney('money', amount)
+            InsertInDatabase(xPlayer.getIdentifier(), '- '..amount..'$ Withdrew')
         end
     end)
-    local data = {
-        firstname = xPlayer.variables.firstName,
-        lastname = xPlayer.variables.lastName,
-        balance = xPlayer.getAccount('bank').money,
-        transactions = transactions
-    }
-    TriggerClientEvent("db-banking:recieveData", source, data)
-end)
 
-RegisterServerEvent("db-banking:logTransaction", function(identifier,msg)
-    exports.oxmysql:insert("INSERT INTO _bankTransactions (identifier, msg)", {identifier, msg})
-end)
-
-RegisterServerEvent("db-banking:withdraw", function(amount)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getAccount('bank').money >= amount then
-        xPlayer.removeAccountMoney('bank', amount)
-        xPlayer.addAccountMoney('money', amount)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "Withdrew $"..amount)
-    end
-end)
-
-RegisterServerEvent("db-banking:deposit", function(amount)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getAccount('money').money >= amount then
-        xPlayer.addAccountMoney('bank', amount)
-        xPlayer.removeAccountMoney('money', amount)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "Deposited $"..amount)
-    end
-end)
-
-RegisterServerEvent("db-banking:transfer", function(data) --TOOD Post Transfers Vals And Use The Data For This
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local xTarget = ESX.GetPlayerFromId(data.target)
-    if xPlayer.getAccount('bank').money >= data.amount then
-        xPlayer.removeAccountMoney('bank', data.amount)
-        xTarget.addAccountMoney('bank', data.amount)
-        TriggerEvent("db-banking:logTransaction", source, xTarget.getIdentifier(), "+ $"..data.amount.." from"..xPlayer.variables.firstName.." "..xPlayer.variables.lastName)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "You Transferred $"..data.amount.." to"..xTarget.firstName)
-    end
-end)
-
-elseif Config["Framework"] == "qb" then
-QBCore = exports["qb-core"]:getCoreObject()
-local transactions
-local PlayerData = QBCore.Functions.GetPlayerData()
-
-RegisterServerEvent("db-banking:openBank", function()
-    local Player = QBCore.Functions.GetPlayer(source)
-    exports.oxmysql:query("SELECT * FROM _bankTransactions WHERE identifier = @identifier", {
-        ['@identifier'] = Player.identifier
-    }, function(r)
-        if r then
-            for _,v in pairs(r) do
-                transactions = v.msg
-            end
+    RegisterNetEvent('db-banking:deposit', function(amount)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer.getAccount('money').money >= amount then
+            xPlayer.addAccountMoney('bank', amount)
+            xPlayer.removeAccountMoney('money', amount)
+            InsertInDatabase(xPlayer.getIdentifier(), '- '..amount..'$ Deposited')
         end
     end)
-    local data = {
-        firstname = PlayerData.charinfo.firstname,
-        lastname = PlayerData.charinfo.lastname,
-        balance = Player.getAccount('bank').money,
-        transactions = transactions
-    }
-    TriggerClientEvent("db-banking:recieveData", source, data)
-end)
 
-RegisterServerEvent("db-banking:logTransaction", function(identifier,msg)
-    exports.oxmysql:insert("INSERT INTO _bankTransactions (identifier, msg)", {identifier, msg})
-end)
+    RegisterNetEvent('db-banking:transfer', function(data)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local xTarget = ESX.GetPlayerFromId(data.target)
+        if xPlayer.getAccount('bank').money >= data.amount then
+            xPlayer.removeAccountMoney('bank', data.amount)
+            xTarget.addAccountMoney('bank', data.amount)
+            InsertInDatabase(xTarget.getIdentifier(), '+ '..data.amount..'$ from'..xPlayer.firstName..' '..xPlayer.variables.lastName)
+            InsertInDatabase(xPlayer.getIdentifier(), '- '..data.amount..'$ to'..xTarget.firstName..' '..xTarget.lastName)
+        end
+    end)
 
-RegisterServerEvent("db-banking:withdraw", function(amount)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getAccount('bank').money >= amount then
-        xPlayer.removeAccountMoney('bank', amount)
-        xPlayer.addAccountMoney('money', amount)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "Withdrew $"..amount)
-    end
-end)
+elseif Config.Framework == 'qb' then
+    local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent("db-banking:deposit", function(amount)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getAccount('money').money >= amount then
-        xPlayer.addAccountMoney('bank', amount)
-        xPlayer.removeAccountMoney('money', amount)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "Deposited $"..amount)
-    end
-end)
+    QBCore.Functions.CreateCallback('db-bank:GetData', function(source, cb)
+        local Player = QBCore.Functions.GetPlayer(source)
+        local PlayerData = Player.PlayerData.charinfo
+        local transactions = {}
+        exports.oxmysql:query('SELECT * FROM db_transactions WHERE identifier = @identifier', {
+            ['@identifier'] = Player.PlayerData.license:gsub('license:','')
+        }, function(result)
+            if result[1] then
+                for i = 0, #result do
+                    table.insert(transactions, result[i])
+                end
+            end
+        end)
+        Wait(100)
+        cb({firstname = PlayerData.firstname, lastname = PlayerData.lastname, balance = Player.Functions.GetMoney('bank'), transactions = transactions})
+    end)
 
-RegisterServerEvent("db-banking:transfer", function(data) --TOOD Post Transfers Vals And Use The Data For This
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local xTarget = ESX.GetPlayerFromId(data.target)
-    if xPlayer.getAccount('bank').money >= data.amount then
-        xPlayer.removeAccountMoney('bank', data.amount)
-        xTarget.addAccountMoney('bank', data.amount)
-        TriggerEvent("db-banking:logTransaction", source, xTarget.getIdentifier(), "+ $"..data.amount.." from"..xPlayer.variables.firstName.." "..xPlayer.variables.lastName)
-        TriggerEvent("db-banking:logTransaction", source, xPlayer.getIdentifier(), "You Transferred $"..data.amount.." to"..xTarget.firstName)
-    end
-end)
+    RegisterNetEvent('db-banking:withdraw', function(amount)
+        amount = tonumber(amount)
+        local Player = QBCore.Functions.GetPlayer(source)
+        if Player.Functions.GetMoney('bank') >= amount then
+            Player.Functions.RemoveMoney('bank', amount, 'withdraw')
+            Player.Functions.AddMoney('money', amount, 'withdraw')
+            InsertInDatabase(Player.PlayerData.license, '- '..amount..'$ Withdrew')
+        end
+    end)
+
+    RegisterNetEvent('db-banking:deposit', function(amount)
+        local Player = QBCore.Functions.GetPlayer(source)
+        if Player.Functions.GetMoney('money') >= amount then
+            Player.Functions.AddMoney('bank', amount, 'deposit')
+            Player.Functions.RemoveMoney('money', amount, 'deposit')
+            InsertInDatabase(Player.PlayerData.license, '- '..amount..'$ Deposited')
+        end
+    end)
+
+    RegisterNetEvent('db-banking:transfer', function(data) --TOOD Post Transfers Vals And Use The Data For This
+        local Player = QBCore.Functions.GetPlayer(source)
+        local Target = QBCore.Functions.GetPlayer(data.target)
+        local PlayerData = Player.PlayerData.charinfo
+        local TargetData = Target.PlayerData.charinfo
+        if Player.Functions.GetMoney('bank') >= data.amount then
+            Player.Functions.RemoveMoney('bank', data.amount, 'transfer')
+            Target.Functions.AddMoney('bank', data.amount, 'transfer')
+            InsertInDatabase(Target.PlayerData.license, '+ '..data.amount..'$ from'..PlayerData.firstName..' '..PlayerData.lastName)
+            InsertInDatabase(Player.PlayerData.license, '- '..data.amount..'$ to'..TargetData.firstName..' '..TargetData.lastName)
+        end
+    end)
+end
+
+function InsertInDatabase(identifier,msg)
+    exports.oxmysql:insert('INSERT INTO db_transactions (identifier, message) VALUES(@identifier, @message)', {
+        ['@identifier'] = tostring(identifier:gsub('license:','')), 
+        ['@message'] = tostring(msg)
+    })
 end
