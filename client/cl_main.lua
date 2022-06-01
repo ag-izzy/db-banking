955496a293ca1c871b2bf5ff39aa6f51fe96a2dd
deivuks420xyz <83653223+deivuks420yxz@users.noreply.github.com>
2022-06-01 23:00:38 +0300

    Added qbus core integration and fixed bugs
    Most off client side code was rewritten.

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,8 +1,22 @@
 Citizen.CreateThread(function()
-    for k,v in pairs(Config["BankZones"]) do
-    exports[Config["TargetName"]]:AddBoxZone("bank"..k, v, {
-        name="bank"..k,
-        useZ = true,
+    for k,v in pairs(Config.BankZones) do
+        exports[Config.TargetName]:AddBoxZone("bank"..k, v, 0.5, 0.5, {
+            name="bank"..k,
+            heading=90,
+            debugPoly=false,
+            minZ=v.z -0.8,
+            maxZ=v.z +0.8,
+        }, {
+            options = {
+                {
+                    event = "db-banking:open",
+                    label = "Open Bank",
+                    icon = "fas fa-hand-holding-usd"
+                }
+            }
+        })
+    end
+    exports[Config.TargetName]:AddTargetModel(Config.ATMS, {
         options = {
             {
                 event = "db-banking:open",
@@ -11,45 +25,79 @@ Citizen.CreateThread(function()
             }
         }
     })
+end)
+
+RegisterNetEvent('db-banking:open', function(data)
+    if Config.Framework == 'qb' then
+        local QBCore = exports['qb-core']:GetCoreObject()
+        QBCore.Functions.TriggerCallback('db-bank:GetData', function(data)
+            SendNUIMessage({
+                show = true,
+                firstname = data.firstname,
+                lastname = data.lastname,
+                balance = data.balance,
+                transactions = data.transactions,
+            })
+            SetNuiFocus(true, true)
+        end)
+    elseif Config.Framework == 'esx' then
+        local ESX = exports['es_extended']:getSharedObject()
+        ESX.TriggerServerCallback('db-bank:GetData', function(data)
+            SendNUIMessage({
+                show = true,
+                firstname = data.firstname,
+                lastname = data.lastname,
+                balance = data.balance,
+                transactions = data.transactions,
+            })
+            SetNuiFocus(true, true)
+        end)
     end
 end)
 
-RegisterNetEvent("db-banking:open", function()
-    OpenGui()    
+RegisterCommand('opbank', function()
+    if Config.Framework == 'qb' then
+        local QBCore = exports['qb-core']:GetCoreObject()
+        QBCore.Functions.TriggerCallback('db-bank:GetData', function(data)
+            SendNUIMessage({
+                show = true,
+                firstname = data.firstname,
+                lastname = data.lastname,
+                balance = data.balance,
+                transactions = data.transactions,
+            })
+            SetNuiFocus(true, true)
+        end)
+    elseif Config.Framework == 'esx' then
+        local ESX = exports['es_extended']:getSharedObject()
+        ESX.TriggerServerCallback('db-bank:GetData', function(data)
+            SendNUIMessage({
+                show = true,
+                firstname = data.firstname,
+                lastname = data.lastname,
+                balance = data.balance,
+                transactions = data.transactions,
+            })
+            SetNuiFocus(true, true)
+        end)
+    end
 end)
 
-function CloseGui()
-    SetNuiFocus(false,false)
-    SendNUIMessage({show = false})
-end
-
-function OpenGui()
-    TriggerServerEvent("db-banking:openBank")
-    SetNuiFocus(true,true)
-    SendNUIMessage({show = true})
-end
-
-RegisterNetEvent("db-banking:recieveData", function(data)
+RegisterNUICallback('close', function()
+    SetNuiFocus(false, false)
     SendNUIMessage({
-        type = "updateData",
-        firstname = data.firstname,
-        lastname = data.lastname,
-        balance = data.balance,
+        show = false,
     })
-    Wait(500)
-    OpenGui()
 end)
 
-RegisterNUICallback("close", CloseGui())
-
-RegisterNUICallback("withdraw", function(data)
-    TriggerServerEvent("db-banking:withdraw", data.amount)
+RegisterNUICallback('withdraw', function(data)
+    TriggerServerEvent('db-banking:withdraw', data.amount)
 end)
 
-RegisterNUICallback("deposit", function(data)
-    TriggerServerEvent("db-banking:deposit", data.amount)
+RegisterNUICallback('deposit', function(data)
+    TriggerServerEvent('db-banking:deposit', data.amount)
 end)
 
-RegisterNUICallback("transfer", function(data)
-    TriggerServerEvent("db-banking:transfer", data)
+RegisterNUICallback('transfer', function(data)
+    TriggerServerEvent('db-banking:transfer', data)
 end)
